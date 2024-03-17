a430b483bee768be1cd54afc8f500210e3defb65
Raphael Alampay <raphael.alampay@gmail.com>
2024-03-17 10:34:32 +0800

    added deployment script

--- src/js/App.js
+++ src/js/App.js
@@ -35,7 +35,9 @@ export default App = () => {
         <nav className="navbar navbar-expand-md navbar-light">
           <div className="container-fluid">
             <h1 className="navbar-brand m-0">
-              <img src={logo} className="header__logo-img" alt="Cloudband Solutions" height="50" fetchpriority="high"/>
+              <a href="#">
+                <img src={logo} className="header__logo-img" alt="Cloudband Solutions" height="50" fetchpriority="high"/>
+              </a>
             </h1>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
