a8d8dae24a3cf1ff2657148e7a2a557c09e266ea
ralampay <raphael.alampay@gmail.com>
2024-08-14 17:54:08 +0800

    upgraded packages and fixed navigation

--- src/js/App.js
+++ src/js/App.js
@@ -17,6 +17,8 @@ import logoRails from "../styles/images/logo/rails.png";
 import logoReact from "../styles/images/logo/react.png";
 import logoRedis from "../styles/images/logo/redis.png";
 
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+
 export default App = () => {
   const [name, setName]           = useState("");
   const [email, setEmail]         = useState("");
@@ -32,18 +34,14 @@ export default App = () => {
   return (
     <React.Fragment>
       <header className="sticky-top bg-white">
-        <nav className="navbar navbar-expand-md navbar-light">
+        <Navbar expand="lg" className="bg-body-primary">
           <div className="container-fluid">
-            <h1 className="navbar-brand m-0">
-              <a href="#">
-                <img src={logo} className="header__logo-img" alt="Cloudband Solutions" height="50" fetchpriority="high"/>
-              </a>
-            </h1>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-              <ul className="navbar-nav main-menu">
+            <Navbar.Brand href="#">
+              <img src={logo} className="header__logo-img" alt="Cloudband Solutions" height="50" fetchpriority="high"/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <ul className="navbar-nav main-menu mx-auto">
                 <li className="nav-item">
                   <a className="nav-link px-lg-4" href="#services">SERVICES</a>
                 </li>
@@ -57,12 +55,12 @@ export default App = () => {
                   <a className="nav-link" href="#contact">CONTACT</a>
                 </li>
               </ul>
-            </div>
-            <div className="d-none d-md-block">
-              <a href="#contact" className="btn btn-accent">GET IN TOUCH</a>
-            </div>
+              <div className="d-none d-md-block">
+                <a href="#contact" className="btn btn-accent">GET IN TOUCH</a>
+              </div>
+            </Navbar.Collapse>
           </div>
-        </nav>
+        </Navbar>
       </header>
       <main>
         <section id="banner" className="banner text-center">
