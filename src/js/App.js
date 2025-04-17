be5e826839fde5a7bd7707065eb2319ba773ee06
Raphael Alampay <raphael.alampay@gmail.com>
2025-04-17 13:55:33 +0800

    cleanup

--- src/js/App.js
+++ src/js/App.js
@@ -21,22 +21,9 @@ import ai from "../styles/images/ai.jpg";
 import hmis from "../styles/images/hmis.jpg";
 
 import Navigation from "./Navigation";
-
-import { submitMessage, hasFormError, getInputClassName, renderInputErrors } from "./Services";
+import ContactForm from "./ContactForm";
 
 export default App = () => {
-  const [name, setName]           = useState("");
-  const [email, setEmail]         = useState("");
-  const [message, setMessage]     = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [errors, setErrors]       = useState({});
-
-  const clearFields = () => {
-    setName("");
-    setEmail("");
-    setMessage("");
-  }
-
   return (
     <React.Fragment>
       <Navigation/>
@@ -315,82 +302,7 @@ export default App = () => {
                 </div>
               </div>
               <div id="contact" className="col-lg-6 mt-5 mt-lg-0">
-                <div className="container--form bg-primary text-light py-3 px-4 rounded shadow-sm">
-                  <h2 className="text-white">
-                    Have a project in mind?
-                  </h2>
-                  <p>
-                    Please fill out the form below to start your project with us. We're excited to work together!
-                  </p>
-                  <div className="mb-3">
-                    <label for="name" className="form-label">Full Name</label>
-                    <input
-                      type="text" 
-                      className={getInputClassName(errors, 'name')}
-                      id="name" 
-                      name="name"
-                      value={name}
-                      disabled={isLoading}
-                      onChange={(event) => { setName(event.target.value); }}
-                    />
-                    {renderInputErrors(errors, 'name')}
-                  </div>
-                  <div className="mb-3">
-                    <label for="email" className="form-label">Email</label>
-                    <input
-                      type="email" 
-                      className={getInputClassName(errors, 'email')}
-                      id="email" 
-                      name="email"
-                      value={email}
-                      disabled={isLoading}
-                      onChange={(event) => { setEmail(event.target.value); }}
-                    />
-                    {renderInputErrors(errors, 'email')}
-                  </div>
-                  <div className="mb-3">
-                    <label for="message" className="form-label">Message</label>
-                    <textarea 
-                      name="message" 
-                      id="message" 
-                      cols="30" 
-                      rows="3" 
-                      className={getInputClassName(errors, 'message')}
-                      value={message} 
-                      disabled={isLoading}
-                      onChange={(event) => { setMessage(event.target.value); }}
-                    />
-                    {renderInputErrors(errors, 'message')}
-                  </div>
-                  <button 
-                    type="submit" 
-                    className="btn btn-lg btn-accent d-block w-100"
-                    disabled={isLoading}
-                    onClick={() => {
-                      setErrors({});
-                      setIsLoading(true);
-
-                      const payload = {
-                        name: name,
-                        email: email,
-                        message: message
-                      }
-
-                      submitMessage(payload).then((res) => {
-                        alert("Thanks for reaching out!");
-                        clearFields();
-                      }).catch((res) => {
-                        console.log("Error in posting message:");
-                        console.log(res.response);
-                        setErrors(res.response.data);
-                      }).finally(() => {
-                        setIsLoading(false);
-                      });
-                    }}
-                  >
-                    SUBMIT
-                  </button>
-                </div>
+                <ContactForm/>
               </div>
             </div>
           </div>
---
+++ src/js/ContactForm.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+
+import { submitMessage, getInputClassName, renderInputErrors } from "./Services";
+
+export default ContactForm = () => {
+  const [name, setName]           = useState("");
+  const [email, setEmail]         = useState("");
+  const [message, setMessage]     = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors]       = useState({});
+
+  const clearFields = () => {
+    setName("");
+    setEmail("");
+    setMessage("");
+  }
+
+  return (
+    <React.Fragment>
+      <div className="container--form bg-primary text-light py-3 px-4 rounded shadow-sm">
+        <h2 className="text-white">
+          Have a project in mind?
+        </h2>
+        <p>
+          Please fill out the form below to start your project with us. We're excited to work together!
+        </p>
+        <div className="mb-3">
+          <label for="name" className="form-label">Full Name</label>
+          <input
+            type="text" 
+            className={getInputClassName(errors, 'name')}
+            id="name" 
+            name="name"
+            value={name}
+            disabled={isLoading}
+            onChange={(event) => { setName(event.target.value); }}
+          />
+          {renderInputErrors(errors, 'name')}
+        </div>
+        <div className="mb-3">
+          <label for="email" className="form-label">Email</label>
+          <input
+            type="email" 
+            className={getInputClassName(errors, 'email')}
+            id="email" 
+            name="email"
+            value={email}
+            disabled={isLoading}
+            onChange={(event) => { setEmail(event.target.value); }}
+          />
+          {renderInputErrors(errors, 'email')}
+        </div>
+        <div className="mb-3">
+          <label for="message" className="form-label">Message</label>
+          <textarea 
+            name="message" 
+            id="message" 
+            cols="30" 
+            rows="3" 
+            className={getInputClassName(errors, 'message')}
+            value={message} 
+            disabled={isLoading}
+            onChange={(event) => { setMessage(event.target.value); }}
+          />
+          {renderInputErrors(errors, 'message')}
+        </div>
+        <button 
+          type="submit" 
+          className="btn btn-lg btn-accent d-block w-100"
+          disabled={isLoading}
+          onClick={() => {
+            setErrors({});
+            setIsLoading(true);
+
+            const payload = {
+              name: name,
+              email: email,
+              message: message
+            }
+
+            submitMessage(payload).then((res) => {
+              alert("Thanks for reaching out!");
+              console.log("res:");
+              console.log(res);
+              clearFields();
+            }).catch((res) => {
+              console.log("Error in posting message:");
+              console.log(res.response);
+              setErrors(res.response.data);
+            }).finally(() => {
+              setIsLoading(false);
+            });
+          }}
+        >
+          SUBMIT
+        </button>
+      </div>
+    </React.Fragment>
+  )
+}
